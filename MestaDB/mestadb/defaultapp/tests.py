e68558a511ce55ff2a390100b9cc163524897d0c
Aapo Rista <aapo.rista@aalto.fi>
2011-09-28 15:02:18 +0300

    Added simple login/logout test cases.

--- MestaDB/mestadb/defaultapp/tests.py
+++ MestaDB/mestadb/defaultapp/tests.py
@@ -6,11 +6,25 @@ Replace this with more appropriate tests for your application.
 """
 
 from django.test import TestCase
+from django.utils import unittest
+from django.test import Client
+from django.contrib.auth.models import User
+from django.core.urlresolvers import reverse
 
+class FPTest(TestCase):
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
+    def setUp(self):
+        self.client = Client()
+        self.username = 'test'
+        self.password = 'test'
+        self.user = User.objects.create(username=self.username)
+        self.user.set_password(self.password)
+        self.user.save()
+
+    def test_user_login(self):
         """
-        Tests that 1 + 1 always equals 2.
+        Tests login, requesting frontpage and logout
         """
-        self.assertEqual(1 + 1, 2)
+        self.assertEqual(self.client.login(username=self.username, password=self.password), True)
+        self.assertEqual(self.client.get(reverse('defaultapp_index')).status_code, 200)
+        self.assertEqual(self.client.get(reverse('defaultapp_logout')).status_code, 302)
