c2074d07f8dc9bbe1e85874542fb04dbc158ae39
Aapo Rista <aapo.rista@aalto.fi>
2011-10-01 15:38:10 +0300

    Added empty urlpattenrs (needed by dynamic inclusion in ROOT_URLCONF).

--- MestaDB/mestadb/location/urls.py
+++ MestaDB/mestadb/location/urls.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 
-#from django.conf.urls.defaults import *
-#from django.conf import settings
-#import views
-#urlpatterns = patterns('',
-#    (r'^/?$', views.index),
-#)
+from django.conf.urls.defaults import *
+from django.conf import settings
+
+# import views
+# Empty urlpatterns is needed due dynamic import in ROOT_URLCONF
+urlpatterns = patterns('',)
