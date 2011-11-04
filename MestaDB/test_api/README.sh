076b3615610dc6c164aead85a8cb2b22ceaf67b0
Aapo Rista <aapo.rista@aalto.fi>
2011-11-04 13:27:16 +0200

    Added simple script to download Django and start server

---
+++ MestaDB/test_api/README.sh
@@ -0,0 +1,16 @@
+
+# Get Django
+wget --no-check-certificate http://www.djangoproject.com/download/1.3.1/tarball/ -O django.tgz
+
+# Extract django directory
+tar zxfv django.tgz Django-1.3.1/django
+
+# Move it to the current directory
+mv Django-1.3.1/django .
+
+# Remove obsolete directory
+rmdir Django-1.3.1
+
+# Start dev server
+python manage.py runserver
+
